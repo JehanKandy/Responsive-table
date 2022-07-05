e85b90716b3c87298ddd54855bc76fb8d952f739
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-06 03:03:27 +0530

    Add files via upload

---
+++ test_responcive_table.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+    <!-- call for data table css  and js-->
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+</head>
+<body>
+
+<body>
+<div class="container">
+    <h2>Data in Table with pagination</h2>
+    <table class="table table-fluid" id="myTable">
+    <thead>
+      <tr>
+        <td>
+          Name
+        </td>
+        <td>
+          Class
+        </td>
+        <td>
+          Address
+        </td>
+        <td>
+          Mobile
+        </td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Kamal</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+      <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+    </tbody>
+    </table>
+</div>
+
+<script src="script.js"></script>
+
+</body>
+</html>
